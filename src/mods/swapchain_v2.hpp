42981033b2453e5fd64e7c35145016dda82b55b2
xiao-mantou <xiao-mantou@users.noreply.github.com>
2026-07-28 22:49:28 +0800

    Render HDR proxy before DLSS FG capture

--- src/mods/swapchain_v2.hpp
+++ src/mods/swapchain_v2.hpp
@@ -134,6 +134,8 @@ inline bool prevent_multiple_flip_swapchains_per_window = true;
 // run normally; only the final proxy draw is skipped once.
 inline std::atomic_bool skip_next_proxy_draw = false;
 inline std::atomic_bool skip_proxy_log_emitted = false;
+inline std::atomic_bool force_next_proxy_draw = false;
+inline std::atomic_uint64_t proxy_draw_serial = 0u;
 
 inline bool bypass_dummy_windows = true;
 [[deprecated("Use use_auto_cloning instead")]]
@@ -1246,7 +1248,8 @@ inline void OnPresent(
   }
   assert(proxy_pass != nullptr);
 
-  if (skip_next_proxy_draw.exchange(false, std::memory_order_acq_rel)) {
+  const bool force_proxy_draw = force_next_proxy_draw.exchange(false, std::memory_order_acq_rel);
+  if (!force_proxy_draw && skip_next_proxy_draw.exchange(false, std::memory_order_acq_rel)) {
     if (!skip_proxy_log_emitted.exchange(true, std::memory_order_acq_rel)) {
       reshade::log::message(
           reshade::log::level::info,
@@ -1258,6 +1261,8 @@ inline void OnPresent(
   if (!proxy_pass->Render(swapchain, queue)) {
     proxy_pass->Destroy(device);
     data->swapchain_proxy_passes.erase(back_buffer_handle);
+  } else {
+    proxy_draw_serial.fetch_add(1u, std::memory_order_release);
   }
 }
 
