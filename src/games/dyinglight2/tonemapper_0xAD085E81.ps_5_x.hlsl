942e0a17505c60a8e56d66d148ea3a002e45080f
xiao-mantou <xiao-mantou@users.noreply.github.com>
2026-08-01 03:30:16 +0800

    Probe DL2 gamma input passthrough

--- src/games/dyinglight2/tonemapper_0xAD085E81.ps_5_x.hlsl
+++ src/games/dyinglight2/tonemapper_0xAD085E81.ps_5_x.hlsl
@@ -5,5 +5,5 @@ void main(
     float4 v0 : SV_POSITION0,
     linear noperspective float2 v1 : TEXCOORD0,
     out float4 o0 : SV_TARGET0) {
-  o0 = float4(6.25, 6.25, 6.25, 1.0);
+  o0 = t0.SampleLevel(s0_s, v1.xy, 0);
 }
