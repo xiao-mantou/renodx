a03da47ac89edfa6a29874562184f837833b3778
xiao-mantou <xiao-mantou@users.noreply.github.com>
2026-09-12 16:03:34 +0800

    fix(f122): trim shader injection to fit 20-dword root constant budget

--- src/games/f122/shared.h
+++ src/games/f122/shared.h
@@ -1,34 +1,12 @@
-#ifndef SRC_GAMES_F12022_SHARED_H_
-#define SRC_GAMES_F12022_SHARED_H_
-
-// #define RENODX_PEAK_WHITE_NITS                 1000.f
-// #define RENODX_DIFFUSE_WHITE_NITS              renodx::color::bt2408::REFERENCE_WHITE
-// #define RENODX_GRAPHICS_WHITE_NITS             renodx::color::bt2408::GRAPHICS_WHITE
-// #define RENODX_COLOR_GRADE_STRENGTH            1.f
-// #define RENODX_TONE_MAP_TYPE                   TONE_MAP_TYPE_RENO_DRT
-// #define RENODX_TONE_MAP_EXPOSURE               1.f
-// #define RENODX_TONE_MAP_HIGHLIGHTS             1.f
-// #define RENODX_TONE_MAP_SHADOWS                1.f
-// #define RENODX_TONE_MAP_CONTRAST               1.f
-// #define RENODX_TONE_MAP_SATURATION             1.f
-// #define RENODX_TONE_MAP_HIGHLIGHT_SATURATION   1.f
-// #define RENODX_TONE_MAP_BLOWOUT                0
-// #define RENODX_TONE_MAP_FLARE                  0
-// #define RENODX_TONE_MAP_HUE_CORRECTION         1.f
-// #define RENODX_TONE_MAP_HUE_SHIFT              0
-// #define RENODX_TONE_MAP_WORKING_COLOR_SPACE    color::convert::COLOR_SPACE_BT709
-// #define RENODX_TONE_MAP_CLAMP_COLOR_SPACE      color::convert::COLOR_SPACE_NONE
-// #define RENODX_TONE_MAP_CLAMP_PEAK             color::convert::COLOR_SPACE_BT709
-// #define RENODX_TONE_MAP_HUE_PROCESSOR          HUE_PROCESSOR_OKLAB
-// #define RENODX_TONE_MAP_PER_CHANNEL            0
-// #define RENODX_GAMMA_CORRECTION                GAMMA_CORRECTION_GAMMA_2_2
+#ifndef SRC_GAMES_F122_SHARED_H_
+#define SRC_GAMES_F122_SHARED_H_
 
 // Must be 32bit aligned
 // Should be 4x32
+// Trimmed to fit the game's smallest pipeline layout root constant budget (20 dwords).
 struct ShaderInjectData {
   float peak_white_nits;
   float diffuse_white_nits;
-  float graphics_white_nits;
   float color_grade_strength;
   float tone_map_type;
   float tone_map_exposure;
@@ -41,22 +19,6 @@ struct ShaderInjectData {
   float tone_map_flare;
   float tone_map_hue_correction;
   float tone_map_hue_shift;
-  float tone_map_working_color_space;
-  float tone_map_clamp_color_space;
-  float tone_map_clamp_peak;
-  float tone_map_hue_processor;
-  float tone_map_per_channel;
-  float gamma_correction;
-  float intermediate_scaling;
-  float intermediate_encoding;
-  float intermediate_color_space;
-  float swap_chain_decoding;
-  float swap_chain_gamma_correction;
-  float swap_chain_custom_color_space;
-  float swap_chain_clamp_color_space;
-  float swap_chain_encoding;
-  float swap_chain_encoding_color_space;
-  float custom_flip_uv_y;
   float debug_mode;
 };
 
@@ -72,15 +34,15 @@ cbuffer shader_injection : register(b13) {
 #define RENODX_TONE_MAP_TYPE                 shader_injection.tone_map_type
 #define RENODX_PEAK_WHITE_NITS               shader_injection.peak_white_nits
 #define RENODX_DIFFUSE_WHITE_NITS            shader_injection.diffuse_white_nits
-#define RENODX_GRAPHICS_WHITE_NITS           shader_injection.graphics_white_nits
-#define RENODX_GAMMA_CORRECTION              shader_injection.gamma_correction
-#define RENODX_TONE_MAP_PER_CHANNEL          shader_injection.tone_map_per_channel
-#define RENODX_TONE_MAP_WORKING_COLOR_SPACE  shader_injection.tone_map_working_color_space
-#define RENODX_TONE_MAP_HUE_PROCESSOR        shader_injection.tone_map_hue_processor
+#define RENODX_GRAPHICS_WHITE_NITS           renodx::color::bt2408::GRAPHICS_WHITE
+#define RENODX_GAMMA_CORRECTION              GAMMA_CORRECTION_NONE
+#define RENODX_TONE_MAP_PER_CHANNEL          0.f
+#define RENODX_TONE_MAP_WORKING_COLOR_SPACE  0.f
+#define RENODX_TONE_MAP_HUE_PROCESSOR        0.f
 #define RENODX_TONE_MAP_HUE_CORRECTION       shader_injection.tone_map_hue_correction
 #define RENODX_TONE_MAP_HUE_SHIFT            shader_injection.tone_map_hue_shift
-#define RENODX_TONE_MAP_CLAMP_COLOR_SPACE    shader_injection.tone_map_clamp_color_space
-#define RENODX_TONE_MAP_CLAMP_PEAK           shader_injection.tone_map_clamp_peak
+#define RENODX_TONE_MAP_CLAMP_COLOR_SPACE    renodx::color::convert::COLOR_SPACE_NONE
+#define RENODX_TONE_MAP_CLAMP_PEAK           renodx::color::convert::COLOR_SPACE_NONE
 #define RENODX_TONE_MAP_EXPOSURE             shader_injection.tone_map_exposure
 #define RENODX_TONE_MAP_HIGHLIGHTS           shader_injection.tone_map_highlights
 #define RENODX_TONE_MAP_SHADOWS              shader_injection.tone_map_shadows
@@ -97,4 +59,4 @@ cbuffer shader_injection : register(b13) {
 
 #endif
 
-#endif  // SRC_GAMES_F12022_SHARED_H_
+#endif  // SRC_GAMES_F122_SHARED_H_
