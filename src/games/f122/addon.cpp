9eaea52459ee47a6eeb784d6147c36c1301936de
xiao-mantou <xiao-mantou@users.noreply.github.com>
2026-09-12 11:30:45 +0800

    fix(f122): use standard compile-time shader replacement instead of forced pipeline cloning

--- src/games/f122/addon.cpp
+++ src/games/f122/addon.cpp
@@ -6,8 +6,6 @@
 #define ImTextureID ImU64
 
 #define DEBUG_LEVEL_0
-#define DEBUG_LEVEL_1
-#define DEBUG_LEVEL_2
 
 #include <deps/imgui/imgui.h>
 #include <include/reshade.hpp>
@@ -289,7 +287,6 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
       if (!reshade::register_addon(h_module)) return FALSE;
 
       if (!initialized) {
-        renodx::mods::shader::force_pipeline_cloning = true;
         renodx::mods::shader::expected_constant_buffer_space = 50;
         renodx::mods::shader::expected_constant_buffer_index = 13;
         renodx::mods::shader::allow_multiple_push_constants = true;
