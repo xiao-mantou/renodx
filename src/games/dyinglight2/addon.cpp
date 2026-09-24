91cd84798329a29ce45ede233555030080dc9856
xiao-mantou <xiao-mantou@users.noreply.github.com>
2026-09-24 11:48:36 +0800

    DL2: test DX12 layout injection independently

--- src/games/dyinglight2/addon.cpp
+++ src/games/dyinglight2/addon.cpp
@@ -213,7 +213,7 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
       renodx::mods::shader::allow_multiple_push_constants = true;
       renodx::mods::shader::force_pipeline_cloning = true;
       renodx::mods::shader::disable_custom_replacements_d3d12 = true;
-      renodx::mods::shader::disable_shader_injection_d3d12 = true;
+      renodx::mods::shader::disable_shader_injection_d3d12 = false;
       renodx::utils::shader::use_replace_on_bind = false;
 
       renodx::mods::swapchain::v2::SetUseHDR10(true);
